abd2de8343f9b92da2744a3aeb55d5f0c08048b0
AqUpd <aquupd@ya.ru>
2021-08-19 16:22:23 +0300

    Changed scale of model

--- src/main/java/com/aqupd/sharkmod/client/renderer/SharkEntityRenderer.java
+++ src/main/java/com/aqupd/sharkmod/client/renderer/SharkEntityRenderer.java
@@ -7,17 +7,22 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class SharkEntityRenderer extends MobEntityRenderer<SharkEntity, SharkEntityModel<SharkEntity>> {
 
     public SharkEntityRenderer(EntityRendererFactory.Context context) {
-        super(context, new SharkEntityModel<SharkEntity>(context.getPart(MainClient.SHARK_MODEL_LAYER)), 0.6f);
+        super(context, new SharkEntityModel<SharkEntity>(context.getPart(MainClient.SHARK_MODEL_LAYER)), 1.0f);
     }
 
     @Override
     public Identifier getTexture(SharkEntity entity) {
         return new Identifier("aqupd", "textures/entity/shark.png");
+    }
+
+    protected void scale(SharkEntity sharkEntity, MatrixStack matrixStack, float f) {
+        matrixStack.scale(2.0F, 2.0F, 2.0F);
     }
 }
